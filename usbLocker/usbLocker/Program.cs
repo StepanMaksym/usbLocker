a7e36b320babc0c4cd55315bdc5919b4a0365c48
unknown <j55555k@gmail.com>
2015-06-14 21:37:18 +0300

    Signed-off-by: unknown <j55555k@gmail.com>

---
+++ usbLocker/usbLocker/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Windows.Forms.VisualStyles;
+
+namespace usbLocker
+{
+    static class Program
+    {
+        [STAThread]
+        [DllImport("user32.dll", SetLastError = true)]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+
+            string result = ReadAllDrives();
+           
+
+            if (result.Contains("test"))
+            {
+                // do nothing
+            }
+            else
+            {
+                //lock computer
+                bool locked = LockWorkStation();
+
+                if (!locked)
+                {
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+            }
+        }
+
+        private static string ReadAllDrives()
+        {
+            string result = string.Empty;
+
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.DriveType.Equals(DriveType.Removable))
+                {
+                    result = result + " " + SetUsbLock(drive);
+                }
+            }
+
+            return result;
+        }
+
+        private static string SetUsbLock(DriveInfo drive)
+        {
+            string resumeFile = drive.Name + @"locker\locker.txt";
+            string result = string.Empty;
+
+            if (File.Exists(resumeFile))
+            {
+                StreamReader myFile = new StreamReader(resumeFile);
+                result = myFile.ReadToEnd();
+                myFile.Close();
+            }
+
+            return result;
+        }
+    }
+}
