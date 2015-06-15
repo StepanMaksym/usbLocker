6d33e3f9e17cf80d13faaa9c332519e99f581fa9
StepanMaksym <j55555k@gmail.com>
2015-06-15 21:58:44 +0300

    02_timerAdded

---
+++ usbLocker/usbLocker/DrivesHandler.cs
@@ -0,0 +1,39 @@
+﻿
+namespace usbLocker
+{
+    /// <summary>
+    /// Handles flash drives
+    /// </summary>
+    class DrivesHandler
+    {
+        //private static string ReadAllDrives()
+        //{
+        //    string result = string.Empty;
+
+        //    foreach (DriveInfo drive in DriveInfo.GetDrives())
+        //    {
+        //        if (drive.DriveType.Equals(DriveType.Removable))
+        //        {
+        //            result = result + " " + SetUsbLock(drive);
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+        //private static string SetUsbLock(DriveInfo drive)
+        //{
+        //    string resumeFile = drive.Name + @"locker\locker.txt";
+        //    string result = string.Empty;
+
+        //    if (File.Exists(resumeFile))
+        //    {
+        //        StreamReader myFile = new StreamReader(resumeFile);
+        //        result = myFile.ReadToEnd();
+        //        myFile.Close();
+        //    }
+
+        //    return result;
+        //}
+    }
+}
--- usbLocker/usbLocker/Form1.cs
+++ usbLocker/usbLocker/Form1.cs
@@ -1,19 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿
+using System;
 using System.Windows.Forms;
 
 namespace usbLocker
 {
     public partial class Form1 : Form
     {
+
+        private Timer timer = new Timer();
+        private int timeLeft = 15;
+
         public Form1()
         {
             InitializeComponent();
+
+
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            timer.Tick += new EventHandler(setLabelText);
+            timer.Start();
+        }
+
+        private void setLabelText(object sender, EventArgs e)
+        {
+            timeLeft--;
+            label1.Text = timeLeft.ToString();
+
+            if (timeLeft <= 0)
+            {
+                timer.Stop();
+            }
         }
     }
 }
